82a8a9c2e1090bb5becce08c08d73d5b262e8845
zqwang <zhqwang9@gmail.com>
2024-07-06 20:49:59 -0700

    minor

--- RL_SLF_Recon.m
+++ RL_SLF_Recon.m
@@ -21,6 +21,7 @@ psfLocations = -200:4:200; % 200 um to 200 um at a step size 4 um
 for p = 1:length(psfLocations)
     psfName{p}=[num2str(psfLocations(p)) '.tiff.tif'];
 end
+loadExistingPSF = true;                    % whether to load PSF mat file saved during previous reconstruction session; if not, PSF will be read and processed from raw PSF images
 
 % data directory
 dataPath = 'data/beads_100ms_ROI_1455_320_LED';
@@ -40,13 +41,12 @@ scaleRatio = 0.2;                          % vertical squeezing ratio
 RESOLUTION = 295;                          % horizontal pixel resolution of each sub-aperture
 PSF_background = 200;                      % PSF data background, not used if loadExistingPSF = true
 background = 1500;                         % raw image background
-loadExistingPSF = true;                    % whether to load PSF mat file saved during previous reconstruction session; if not, PSF will be read and processed from raw PSF images
 
 % deconvolution configurations
 iter = 16;                                 % iteration number
 intensityScale = 0.1;                      % global intensity scaling of recontruction results before saving
 debug = false;                             % debug mode; if enabled, it will save and show intermediate results
-usingGPU = false;                           % whether to use GPU; highly recommended 
+usingGPU = true;                           % whether to use GPU; highly recommended 
 conv_type = 'simple_fft';                  % convolution implementation: 'space_domain', space domain convolution, slowest; 'fft', fft based convolution; 'simple_fft', simplified fft based convolution, might be subject to artifacts (rarely though), fastest 
 
 
